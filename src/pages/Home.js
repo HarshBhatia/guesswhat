d44192041146de341b5e9f6a38dcc0c401178ffe
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-04 14:51:47 +0530

    ...

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,1 +1,20 @@
-export default null;
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <label style={{ marginLeft: "20px" }}>Game ID</label>
+      <input
+        style={{ marginLeft: "30px" }}
+        type="text"
+        placeholder="enter your Game ID"
+      />
+      <div style={{ marginTop: "20px" }}>
+        <button style={{ marginLeft: "20px" }}>Join Game</button>
+        <button style={{ marginLeft: "30px" }}>Create Game</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
